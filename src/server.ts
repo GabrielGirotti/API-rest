214fb41972c99af219e82b40cf28e8c037cebe96
GabrielGirotti <girottigabriel@gmail.com>
2024-08-03 09:48:58 -0300

    using CORS

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import router from "./router";
 import db from "./config/db";
 import colors from "colors";
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec, {swaggerOptions} from "./config/swagger";
+import swaggerSpec, { swaggerOptions } from "./config/swagger";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 
 export async function connectDB() {
   try {
@@ -19,10 +21,27 @@ connectDB();
 
 const server = express();
 
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
+server.use(cors(corsOptions));
+
 server.use(express.json());
+
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
-server.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
+server.use(
+  "/doc",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerOptions)
+);
 
 export default server;
